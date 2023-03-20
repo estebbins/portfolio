fa456f3c9fe557489ab8c876b1b9cc441998f436
estebbins <stebbins.e@gmail.com>
2023-03-20 10:25:47 -0400

    initializes repository

---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+
+
+const Greeting = (props) => {
+    return (
+        <h1>Hi, I'm Emily</h1>
+    )
+}
+
+export default Greeting
---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+
+
+const Links = (props) => {
+    return (
+        <a href="https://www.linkedin.com/in/emilyastebbins/">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/emilyastebbins/">GitHub</a>
+    )
+}
+
+export default Links
