1f0f0d939c4a406544ef4705355ae7f72892fd77
estebbins <stebbins.e@gmail.com>
2023-03-22 12:18:59 -0400

    fixes github link

--- src/components/Links.js
+++ src/components/Links.js
@@ -7,7 +7,7 @@ const Links = (props) => {
     return (
         <>
             <a href="https://www.linkedin.com/in/emilyastebbins/"><img className="LI-Logo" src="./LI-Logo.png" alt="LinkedIn Logo"/></a>
-            <a href="https://www.linkedin.com/in/emilyastebbins/"><img className="LI-Logo" src="./github-mark.png" alt="GitHub Logo"/></a>
+            <a href="https://github.com/estebbins"><img className="LI-Logo" src="./github-mark.png" alt="GitHub Logo"/></a>
             <a href="./Emily_Stebbins_Resume_032023.pdf" target="_blank"><img className="resume" src="https://cdn-icons-png.flaticon.com/512/942/942748.png" alt="Flaticon resume"/></a>
             <Button className="contact-btn"><img className="resume" src="https://cdn-icons-png.flaticon.com/512/3771/3771518.png" alt="Flaticon resume"/></Button>
             <Modal show={show}>
