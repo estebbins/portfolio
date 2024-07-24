b22f2ce1875e7268d330c29d90384ebfb9d905ca
estebbins <stebbins.e@gmail.com>
2024-07-24 18:03:11 -0400

    adds skills and updates looping to reflect these changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,10 @@
 import { Container, Col } from "react-bootstrap"
 
 const Skills = (props) => {
-    let skillsArray = ["Python", "Node", "Express", "Django", "LiquidJS", "React", "HTML", "CSS", "Bootstrap", "MongoDB", "SQL", "JavaScript", "Python", "Node", "Express", "Django", "LiquidJS", "React"]
+    let skillsArray = ["Epic Cadence", "Excel", "Avaya CMS", "Avaya AACC", "Lean", "Analytics", "GitHub", "Python", "Node", "Express", "Django", "LiquidJS", "React", "HTML", "CSS", "Bootstrap", "MongoDB", "JavaScript", "SQL", "UEFN", "Unreal Editor", "Verse", "Epic Cadence", "Excel", "Avaya CMS", "Avaya AACC", "Lean", "Analytics", "GitHub", "Python", "Node", "Express", "Django"
+    ]
+    // Need to put in "skill first" p below, the final skill before the looping. Loop with half of the main array (I think)
+    // ["Python", "Node", "Express", "Django", "LiquidJS", "React", "HTML", "CSS", "Bootstrap", "MongoDB", "SQL", "JavaScript", "UEFN", "Verse", "Unreal Editor", "Epic Cadence", "Advanced Excel", "Avaya CMS", "Avaya AACC", "Lean", "Analytics", "GitHub", "Python", "Node", "Express", "Django", "LiquidJS", "React"]
 
     let skills = skillsArray.map((skill, i) => (
         <p key={i} className="skill text-center">{skill}</p>
@@ -10,7 +13,7 @@ const Skills = (props) => {
     return (
         <>
             <Container className="skillbanner skilllist" fluid>
-                <p className="skill first">JavaScript</p>
+                <p className="skill first">Verse</p>
                 {skills}
             </Container>
         </>
