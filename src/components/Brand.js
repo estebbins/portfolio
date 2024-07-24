efd6253ae08d90999e23b0282b659d9760461459
estebbins <stebbins.e@gmail.com>
2024-07-24 17:09:39 -0400

    updates links file to swap out linkedin and resume icons, add fortnite icon and link, and remove contact link, does not make resume up to date

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,7 +3,7 @@
 const Brand = (props) => {
     return (
         <p className="brand-statement">
-            I am a Healthcare Executive turned Software Engineer that is a creative and humanistic strategic-thinker with an eye for detail and a passion for making the human experience more pleasant. I take complex problems and break them down into executable steps and bring together the resources and skillsets needed to create the best solution. My professional experiences as a product owner in a healthcare provider organization informs my drive to create better systems that support seamless interactions for internal and external customers. My personal experiences traveling the world provides me perspective and cultural competency to excel at working with diverse team members and customers.
+            I am a Healthcare Executive turned Software Engineer and Video Game Developer that is a creative and humanistic strategic thinker with an eye for detail and a passion for making the human experience more pleasant. I take complex problems and break them down into executable steps and bring together the resources and skill sets needed to create the best solution. My professional experiences as a product owner in a healthcare provider organization informs my drive to create better systems that support seamless interactions for internal and external customers. My personal experiences traveling the world and living overseas provides me perspective and cultural competency to excel at working with diverse team members and customers. 
         </p>
     )
 }
