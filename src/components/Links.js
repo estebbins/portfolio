51aa65ac2b26dcb767129bcfae0d3428c4cc31f6
estebbins <stebbins.e@gmail.com>
2024-07-26 11:10:03 -0400

    updated resume link to more recent resume

--- src/components/Links.js
+++ src/components/Links.js
@@ -9,7 +9,7 @@ const Links = (props) => {
             <a href="https://www.linkedin.com/in/emilyastebbins/"><img className="LI-Logo" src="./LI-In-Bug.png" alt="LinkedIn Logo"/></a>
             <a href="https://github.com/estebbins"><img className="LI-Logo" src="./github-mark.png" alt="GitHub Logo"/></a>
             <a href="https://www.fortnite.com/@ghost.mouse" target="_blank"><img className="fortnite" src="./fortnite_icon.png" alt="Created in Fortnite icon"/></a>
-            <a href="./Emily_Stebbins_Resume_032023.pdf" target="_blank"><img className="resume" src="./resume-icon.png" alt="resume icon"/></a>
+            <a href="./Emily_Stebbins_Resume_072024.pdf" target="_blank"><img className="resume" src="./resume-icon.png" alt="resume icon"/></a>
             {/* <Button className="contact-btn"><img className="resume" src="https://cdn-icons-png.flaticon.com/512/3771/3771518.png" alt="Flaticon resume"/></Button>
             <Modal show={show}>
                 <Contact />
